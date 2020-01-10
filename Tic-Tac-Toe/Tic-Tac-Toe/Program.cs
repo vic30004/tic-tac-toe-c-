cd55117507bebffcb902a13ae55456751aba8953
vic30004 <victor_993@msn.com>
2020-01-10 18:24:17 +0200

    basic setup

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayBoard();   
+        }
+
+        static void DisplayBoard()
+        {
+            int[,] board = {{1,2,3},
+                            { 4,5,6},
+                            {7,8,9 }
+            };
+
+            foreach(int x in board)
+            {
+                Console.WriteLine( "\t{0}__|\n|",x);
+            }
+            
+            
+            
+        }
+    }
+}

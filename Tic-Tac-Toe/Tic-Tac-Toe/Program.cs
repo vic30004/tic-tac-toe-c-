cfbf0b9591e756e9158214fd992f318705c0ec4e
vic30004 <victor_993@msn.com>
2020-01-10 20:01:53 +0200

    started implementing the logic for the game

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -4,25 +4,60 @@ namespace Tic_Tac_Toe
 {
     class Program
     {
+        static char[,] board = {
+            {'1','2','3'},
+                            
+            { '4','5','6'},
+                            
+            {'7','8','9' } 
+        };
         static void Main(string[] args)
         {
-            DisplayBoard();   
-        }
+            int player = 2;
+            int input = 0;
+            bool checkIfCorrect = true;
+            DisplayBoard();
 
-        static void DisplayBoard()
-        {
-            int[,] board = {{1,2,3},
-                            { 4,5,6},
-                            {7,8,9 }
-            };
+            do {
 
-            foreach(int x in board)
-            {
-                Console.WriteLine( "\t{0}__|\n|",x);
+                if (player == 2)
+                {
+                    player = 1;
+                }else
+                {
+                    player = 2;
+                }
+            
+                switch (player)
+                {
+                    case 1:
+                        {
+                            switch (input)
+                            {
+                                case 1: board[0, 0] = 'x'; break;
+                            }
+                        }
+                        break;
+                }
+
+
             }
-            
-            
-            
+
+            while( true );
         }
-    }
+
+        public static void DisplayBoard()
+        {
+ 
+
+                Console.WriteLine( "{0}|{1}|{2}\n{3}|{4}|{5}\n{6}|{7}|{8}",board[0,0], board[0,1],board[0,2],board[1,0],board[1,1],board[1,2], board[2,0],board[2,1],board[2,2] );
+          
+            
+            
+       
+            }
+        }
+
+        
+    
 }

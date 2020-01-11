a10411a8199106f244332640040141d69bf34bf7
vic30004 <victor_993@msn.com>
2020-01-11 12:10:32 +0200

    added a function with the switch statement for dry code

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -25,49 +25,16 @@ namespace Tic_Tac_Toe
                 if (player == 2)
                 {
                     player = 1;
-                }else
+                    EnterXorO(player, input);
+
+                }
+                else if(player==1)
                 {
                     player = 2;
+                    EnterXorO(player, input);
                 }
             
-                switch (player)
-                {
-                    case 1:
-                        {
-                            switch (input)
-                            {
-                                case 1: board[0, 0] = 'x';break;
-                                case 2: board[0,1] = 'x';break;
-                                case 3: board[0, 2] = 'x';break;
-                                case 4: board[1, 0] = 'x';break;
-                                case 5: board[1, 1] = 'x';break;
-                                case 6: board[1, 2] = 'x';break;
-                                case 7: board[2, 0] = 'x';break;
-                                case 8: board[2, 1] = 'x';break;
-                                case 9: board[2, 2] = 'x';break;
-
-                            }
-                        }
-
-                        break;
-                    case 2:
-                        {
-                            switch(input)
-                            {
-                                case 1: board[0, 0] = 'o'; break;
-                                case 2: board[0, 1] = 'o'; break;
-                                case 3: board[0, 2] = 'o'; break;
-                                case 4: board[1, 0] = 'o'; break;
-                                case 5: board[1, 1] = 'o'; break;
-                                case 6: board[1, 2] = 'o'; break;
-                                case 7: board[2, 0] = 'o'; break;
-                                case 8: board[2, 1] = 'o'; break;
-                                case 9: board[2, 2] = 'o'; break;
-                            }
-                        }
-                        break;
-                }
-
+                
 
             }
 
@@ -81,6 +48,30 @@ namespace Tic_Tac_Toe
                 Console.WriteLine( "{0}|{1}|{2}\n{3}|{4}|{5}\n{6}|{7}|{8}",board[0,0], board[0,1],board[0,2],board[1,0],board[1,1],board[1,2], board[2,0],board[2,1],board[2,2] );
        
             }
+        
+        public static void EnterXorO(int player, int input) {
+
+            char playerSign = ' ';
+
+            if (player == 1)
+                playerSign = 'x';
+            else if (player == 2)
+                playerSign = 'O';
+            switch (player)
+            {
+                case 1: board[0, 0] = playerSign; break;
+                case 2: board[0, 1] = playerSign; break;
+                case 3: board[0, 2] = playerSign; break;
+                case 4: board[1, 0] = playerSign; break;
+                case 5: board[1, 1] = playerSign; break;
+                case 6: board[1, 2] = playerSign; break;
+                case 7: board[2, 0] = playerSign; break;
+                case 8: board[2, 1] = playerSign; break;
+                case 9: board[2, 2] = playerSign; break;
+            }
+        }
+                        
+        
         }
 
         

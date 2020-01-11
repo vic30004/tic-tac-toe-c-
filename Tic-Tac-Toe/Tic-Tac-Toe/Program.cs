f23eeeacf26b8b3b89ccd6d258185c4d53449c60
vic30004 <victor_993@msn.com>
2020-01-11 11:55:12 +0200

    finished adding thr switch statements

--- Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Tic-Tac-Toe/Program.cs
@@ -18,6 +18,8 @@ namespace Tic_Tac_Toe
             bool checkIfCorrect = true;
             DisplayBoard();
 
+            
+
             do {
 
                 if (player == 2)
@@ -34,7 +36,33 @@ namespace Tic_Tac_Toe
                         {
                             switch (input)
                             {
-                                case 1: board[0, 0] = 'x'; break;
+                                case 1: board[0, 0] = 'x';break;
+                                case 2: board[0,1] = 'x';break;
+                                case 3: board[0, 2] = 'x';break;
+                                case 4: board[1, 0] = 'x';break;
+                                case 5: board[1, 1] = 'x';break;
+                                case 6: board[1, 2] = 'x';break;
+                                case 7: board[2, 0] = 'x';break;
+                                case 8: board[2, 1] = 'x';break;
+                                case 9: board[2, 2] = 'x';break;
+
+                            }
+                        }
+
+                        break;
+                    case 2:
+                        {
+                            switch(input)
+                            {
+                                case 1: board[0, 0] = 'o'; break;
+                                case 2: board[0, 1] = 'o'; break;
+                                case 3: board[0, 2] = 'o'; break;
+                                case 4: board[1, 0] = 'o'; break;
+                                case 5: board[1, 1] = 'o'; break;
+                                case 6: board[1, 2] = 'o'; break;
+                                case 7: board[2, 0] = 'o'; break;
+                                case 8: board[2, 1] = 'o'; break;
+                                case 9: board[2, 2] = 'o'; break;
                             }
                         }
                         break;
@@ -51,9 +79,6 @@ namespace Tic_Tac_Toe
  
 
                 Console.WriteLine( "{0}|{1}|{2}\n{3}|{4}|{5}\n{6}|{7}|{8}",board[0,0], board[0,1],board[0,2],board[1,0],board[1,1],board[1,2], board[2,0],board[2,1],board[2,2] );
-          
-            
-            
        
             }
         }
